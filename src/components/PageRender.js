79f5eea25cbcf1357cd80c7fc11515a760599dfe
Heriberto lopez <alexito2002.ll@gmail.com>
2022-04-18 17:28:40 -0600

    added an animated block

--- src/components/PageRender.js
+++ src/components/PageRender.js
@@ -1,7 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Navigation from "./Navigation";
+import BIRDS from "vanta/dist/vanta.birds.min";
+import NET from "vanta/dist/vanta.net.min";
 
 import Home from "./pages/Home";
 import Faq from "./pages/Faq";
@@ -30,6 +32,35 @@ export default function PageRender() {
     }
   };
   const handlePageChange = (page) => setCurrentPage(page);
+  const MyComponent = (props) => {
+    const [vantaEffect, setVantaEffect] = useState(0);
+    const myRef = useRef(null);
+    useEffect(() => {
+      if (!vantaEffect) {
+        setVantaEffect(
+          NET({
+            el: myRef.current,
+            mouseControls: true,
+            touchControls: true,
+            gyroControls: false,
+            minHeight: 500.0,
+            minWidth: 200.0,
+            scale: 1.0,
+            scaleMobile: 1.0,
+          })
+        );
+      }
+      return () => {
+        if (vantaEffect) vantaEffect.destroy();
+      };
+    }, [vantaEffect]);
+    return (
+      <div className="whiteText" ref={myRef}>
+        We can always meet your needs and our prices vary based on your needs
+        and budget
+      </div>
+    );
+  };
   return (
     <div className="parent">
       <Header />
@@ -38,6 +69,8 @@ export default function PageRender() {
         handlePageChange={handlePageChange}
       />
 
+      {MyComponent()}
+      <div className="gap"></div>
       <div className="content">{renderPage()}</div>
       <footer className="footer">
         <Footer />
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,10 +1,29 @@
-import React from "react";
-
+import React, { useState, useEffect, useRef } from "react";
+import BIRDS from "vanta/dist/vanta.birds.min";
 import pht1 from "../../images/pht1.jpg";
+// Make sure window.THREE is defined, e.g. by including three.min.js in the document head using a <script> tag
 
 export default function Home() {
+  const MyComponent = (props) => {
+    const [vantaEffect, setVantaEffect] = useState(0);
+    const myRef = useRef(null);
+    useEffect(() => {
+      if (!vantaEffect) {
+        setVantaEffect(
+          BIRDS({
+            el: myRef.current,
+          })
+        );
+      }
+      return () => {
+        if (vantaEffect) vantaEffect.destroy();
+      };
+    }, [vantaEffect]);
+    return <div ref={myRef}>Foreground content goes here</div>;
+  };
   return (
     <>
+      {/* {MyComponent()} */}
       <p>This is the home page</p>
       <h3>here are some examples of our builds:</h3>
       <img src={pht1} alt="moutain" />
