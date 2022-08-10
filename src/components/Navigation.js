f9eb8178c374a296b35611bea0f23dff5530ab3f
Heriberto lopez <alexito2002.ll@gmail.com>
2022-08-10 11:46:04 -0600

    fixed the pages in navbar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,24 +3,40 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 
-function CollapsibleExample() {
+function Navigation({ currentPage, handlePageChange }) {
   return (
     <Navbar sticky="top" collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
-        <Navbar.Brand href="/">My Website</Navbar.Brand>
+        <Navbar.Brand href="/" onClick={() => handlePageChange("Home")}>
+          My Website
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto justify-content-end flex-grow-1">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#contact">Contact Us</Nav.Link>
+            <Nav.Link href="#home" onClick={() => handlePageChange("Home")}>
+              Home
+            </Nav.Link>
+            <Nav.Link
+              href="#contact"
+              onClick={() => handlePageChange("Contact")}
+            >
+              Contact Us
+            </Nav.Link>
             <NavDropdown title="Services" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#services">All Services</NavDropdown.Item>
+              <NavDropdown.Item
+                href="#services"
+                onClick={() => handlePageChange("Services")}
+              >
+                All Services
+              </NavDropdown.Item>
               <NavDropdown.Divider />
               <NavDropdown.Item href="#service1">Service1</NavDropdown.Item>
               <NavDropdown.Item href="#service2">Service2</NavDropdown.Item>
               <NavDropdown.Item href="#service3">service3</NavDropdown.Item>
             </NavDropdown>
-            <Nav.Link href="/faq">FAQ</Nav.Link>
+            <Nav.Link href="#faq" onClick={() => handlePageChange("Faq")}>
+              FAQ
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
@@ -28,4 +44,4 @@ function CollapsibleExample() {
   );
 }
 
-export default CollapsibleExample;
+export default Navigation;
